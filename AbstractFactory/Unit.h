5f45d3db04ed79973c4f4c920d82794cd6ab77dc
D4rksoon <Asdreem2539@gmail.com>
2024-05-10 20:32:57 +0800

    Описание кода, больше тестов

--- AbstractFactory/Unit.h
+++ AbstractFactory/Unit.h
@@ -6,7 +6,7 @@
 
 class Unit {
 public:
-    using Flags = unsigned int;
+    using Flags = unsigned int; // переобозначение
 public:
     virtual ~Unit() = default;
     virtual void add( const std::shared_ptr< Unit >& , Flags ) {
@@ -26,54 +26,60 @@ protected:
     }
 };
 
-
+// класс нашего класса
 class ClassUnit : public Unit
 {
 public:
-    enum AccessModifier {
-        PUBLIC,
-        PROTECTED,
+    enum AccessModifier { // Перечисление (определение типов)
+        PUBLIC, // эквивалент 0
+        PROTECTED, // эквивалент 1
         PRIVATE
     };
     static const std::vector< std::string > ACCESS_MODIFIERS;
 public:
+    // explicit - запрещает неявные преобразования
     explicit ClassUnit( const std::string& name ) : m_name( name ) {
         m_fields.resize( ACCESS_MODIFIERS.size() );
     }
+    // Flags - переобозначили (unsigned int)
     void add( const std::shared_ptr< Unit >& unit, Flags flags ) {
-        int accessModifier = PRIVATE;
-        if( flags < ACCESS_MODIFIERS.size() ) {
-            accessModifier = flags;
+        // По умолчанию accessModifier стоит PUBLIC
+        int accessModifier = PRIVATE; // По умолчанию ставим PRIVATE
+        if( flags < ACCESS_MODIFIERS.size() ) { // Если flags меньше вектора известных нам
+            accessModifier = flags;             // модификаторов ставим модификатор
         }
         m_fields[ accessModifier ].push_back( unit );
     }
+    // level - количестов отступов level * shift
     std::string compile( unsigned int level = 0 ) const
     {
         std::string result = generateShift( level ) + "class " + m_name + " {\n";
+        // Выводится в консоль начиная с public функций, заканчивая private
         for( size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i ) {
             if( m_fields[ i ].empty() ) {
-                continue;
+                continue; // если вектор пуст пропускаем, идем на следующий
             }
             result += ACCESS_MODIFIERS[ i ] + ":\n";
-            for( const auto& f : m_fields[ i ] ) {
+            for( const auto& f : m_fields[ i ] ) { // идем по элементам вектора
+                // f это MethodUnit использует compile из класса MethodUnit
                 result += f->compile( level + 1 );
             }
-            result += "\n";
+            result += "\n"; // после обхода вектора в конце отступ
         }
-        result += generateShift( level ) + "};\n";
+        result += generateShift( level ) + "};\n"; // в самом конце класса отступ
         return result;
     }
 private:
     std::string m_name;
     using Fields = std::vector< std::shared_ptr< Unit > >;
-    std::vector< Fields > m_fields;
-};
+    std::vector< Fields > m_fields; // вектор из 3 векторов
+}; // Выводится в консоль начиная с public функций, заканчивая private (можно поменять)
 const std::vector< std::string > ClassUnit::ACCESS_MODIFIERS = { "public", "protected", "private" };
 
-
+// Класс функций (методов)
 class MethodUnit : public Unit {
 public:
-    enum Modifier {
+    enum Modifier { // Перечисление (битовые флаги)
         STATIC = 1,
         CONST = 1 << 1,
         VIRTUAL = 1 << 2
@@ -81,24 +87,33 @@ public:
 public:
     MethodUnit( const std::string& name, const std::string& returnType, Flags flags ) :
         m_name( name ), m_returnType( returnType ), m_flags( flags ) { }
-
+    // добваить в метод другие методы
+    // Flags - не знаю зачем нужен. Ни на что не влияет (можно удалить)
     void add( const std::shared_ptr< Unit >& unit, Flags /* flags */ = 0 ) {
         m_body.push_back( unit );
     }
+    // вывести метод
     std::string compile( unsigned int level = 0 ) const {
         std::string result = generateShift( level );
+        // Если флаг совпадает, добавляем
         if( m_flags & STATIC ) {
             result += "static ";
         } else if( m_flags & VIRTUAL ) {
             result += "virtual ";
         }
-        result += m_returnType + " ";
-        result += m_name + "()";
+
+        result += m_returnType + " "; // Тип метода
+        result += m_name + "()"; // Имя метода
+
+        // Модификаторы, которые идут после объявления
         if( m_flags & CONST ) {
             result += " const";
         }
         result += " {\n";
+        // Если в метод добавили ещё метод (проходим по телу метода)
         for( const auto& b : m_body ) {
+            // b - MethodUnit | PrintOperatorUnit
+            // Отступ увеличили на один shift
             result += b->compile( level + 1 );
         }
         result += generateShift( level ) + "}\n";
@@ -111,7 +126,7 @@ private:
     std::vector< std::shared_ptr< Unit > > m_body;
 };
 
-
+// Добавляет в метод описание printf("text")
 class PrintOperatorUnit : public Unit {
 public:
     explicit PrintOperatorUnit( const std::string& text ) : m_text( text ) { }
--- AbstractFactory/main.cpp
+++ AbstractFactory/main.cpp
@@ -5,7 +5,7 @@
 std::string generateProgram() {
     ClassUnit myClass( "MyClass" );
     myClass.add(
-        std::make_shared< MethodUnit >( "testFunc1", "void", 0 ),
+        std::make_shared< MethodUnit >( "testFunc1", "void",  0),
         ClassUnit::PUBLIC
         );
     myClass.add(
@@ -21,6 +21,17 @@ std::string generateProgram() {
                                                MethodUnit::STATIC );
     method->add( std::make_shared< PrintOperatorUnit >( R"(Hello, world!\n)" ) );
     myClass.add( method, ClassUnit::PROTECTED );
+
+
+    auto method2 = std::make_shared< MethodUnit >( "TTTT1", "void", 0);
+    method2->add( std::make_shared< MethodUnit >( "TTTT2", "void", 1 )/*, 2*/); // Указывать модификацию необязательно
+    method2->add( std::make_shared< MethodUnit >( "TTTT3", "void", 2 )/*, 2*/);
+    auto method3 = std::make_shared< MethodUnit >( "TTTT4", "void", 0);
+    method3->add( std::make_shared< MethodUnit >( "TTTT5", "void", 0 ));
+    method2->add(method3);
+    myClass.add( method2, ClassUnit::PROTECTED );
+
+
     return myClass.compile();
 }
 
